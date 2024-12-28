dab0d18aa2f3987e736db3ef544e9f361ba20583
kris aziabor <krisaaziabor@gmail.com>
2024-12-28 11:48:16 -0500

    installed table primitives

--- labs-book/src/app/admin/components/preorderList.tsx
+++ labs-book/src/app/admin/components/preorderList.tsx
@@ -1,25 +1,65 @@
-import React, { useEffect, useState } from 'react';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import React, { useEffect, useState } from "react";
 
 const PreorderList = () => {
-    const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
 
-    const displayPreorders = async () => {
-        const response = await fetch(`/admin/api`);
-        const text = await response.text();
-        setGreeting(text);
-    };
+  const displayPreorders = async () => {
+    const response = await fetch(`/admin/api`);
+    const text = await response.text();
+    console.log(response)
+    setGreeting(text);
+  };
 
-    useEffect(() => {
-        displayPreorders();
-    }, []);
+  useEffect(() => {
+    displayPreorders();
+  }, []);
 
-    return (
-        <div>
-            <h1>DAY Book 2024 Admin Page</h1>
-            <h2>Preorders</h2>
-            <p>{greeting}</p>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <h1>DAY Book 2024 Admin Page</h1>
+        <h2>Preorders</h2>
+        <p>{greeting}</p>
+      </div>
+      <br />
+      <div>
+        <Table>
+          <TableCaption>A list of our pre-orders.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">#</TableHead>
+              <TableHead>Order date</TableHead>
+              <TableHead>First name</TableHead>
+              <TableHead>Last name</TableHead>
+              <TableHead>Residential college</TableHead>
+              <TableHead>Payment method</TableHead>
+              <TableHead>Username/phone number</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>12/25/2024</TableCell>
+              <TableCell>John</TableCell>
+              <TableCell>Doe</TableCell>
+              <TableCell>Benjamin Franklin</TableCell>
+              <TableCell>Venmo</TableCell>
+              <TableCell>johndoe</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default PreorderList;

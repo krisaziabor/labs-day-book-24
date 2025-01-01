7cd942296c82786e4f486cf911ade6f47432821f
kris aziabor <krisaaziabor@gmail.com>
2025-01-01 10:15:56 -0500

    added 3 new lambda functions

---
+++ lambda/archive/pendingorders-lambda-nodejs18.x/hello-world/index.mjs
@@ -0,0 +1,44 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    },
+});
+
+export async function handler(event) {
+    const query = 'SELECT * FROM preorders WHERE pending = true';
+
+    try {
+        const result = await pool.query(query);
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify(result.rows),
+        };
+    } catch (error) {
+        console.log('Error executing query', error);
+        return {
+            'statusCode': 500,
+            'body': JSON.stringify(error),
+        };
+    }
+}
